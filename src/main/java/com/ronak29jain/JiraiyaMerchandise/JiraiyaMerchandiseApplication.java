d11dcde0d0ba388bb5a14b9feb24460faea934fb
ronak29jain <ronak290jain@gmail.com>
2024-05-15 23:00:50 +0530

    initial commit

---
+++ src/main/java/com/ronak29jain/JiraiyaMerchandise/JiraiyaMerchandiseApplication.java
@@ -0,0 +1,13 @@
+package com.ronak29jain.JiraiyaMerchandise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraiyaMerchandiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraiyaMerchandiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronak29jain/JiraiyaMerchandise/JiraiyaMerchandiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronak29jain.JiraiyaMerchandise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraiyaMerchandiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
